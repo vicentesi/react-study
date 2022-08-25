eb9c3b5270e316fc76eba1da94374c0b928bf7fd
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2022-08-25 16:12:23 -0300

    add components

---
+++ theme-switcher/src/components/Header/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 60px;
+  background-color: #ff57d9;
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+`
