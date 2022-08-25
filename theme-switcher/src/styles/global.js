be5b454d71fdf25b4be9d3309158f787fd4ce6b6
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2022-08-25 15:51:36 -0300

    Add global style

---
+++ theme-switcher/src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #e6e7e8;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
