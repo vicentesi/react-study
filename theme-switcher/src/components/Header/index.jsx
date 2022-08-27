9852779671889113de5df69799a9b4fb7b9014bb
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2022-08-27 19:46:04 -0300

    Add react switch

--- theme-switcher/src/components/Header/index.jsx
+++ theme-switcher/src/components/Header/index.jsx
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+
 import * as S from './style'
 
 const Header = () => {
+  const { colors } = useContext(ThemeContext);
+
   return (
     <S.Container>
       <h2>Header</h2>
-      <h2>Switch</h2>
+
+      <Switch
+        onChange={() => { }}
+        checked={true}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        onColor={colors.secondary}
+        offColor={colors.background}
+      />
     </S.Container>
   )
 }
